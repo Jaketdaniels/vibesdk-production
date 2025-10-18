87314dc3466f1c66dbea787c1aa1cecae94b4817
Jaketdaniels <jaketdaniels95@gmail.com>
2025-10-18 20:27:57 +1100

    Refactor variable name from 'rand' to 'rnd'

--- src/utils/generateId.ts
+++ src/utils/generateId.ts
@@ -1,12 +1,13 @@
-// src/utils/generateId.ts
-
 /**
  * Generate a unique identifier with optional prefix.
+ *
+ * @param prefix - Optional string prefix.
+ * @returns A unique string ID.
  */
 export function generateId(prefix = ''): string {
   const ts = Date.now().toString(36);
-  const rand = Math.random().toString(36).substring(2, 9);
-  return `${prefix}${ts}-${rand}`;
+  const rnd = Math.random().toString(36).substring(2, 9);
+  return `${prefix}${ts}-${rnd}`;
 }
 
 export default generateId;
